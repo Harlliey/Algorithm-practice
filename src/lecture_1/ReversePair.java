f8bf4f082737195d4430e05483a266173cd9ecb7
Harlliey <276095274@qq.com>
2019-07-07 18:16:35 +0800

    lecture-1

--- src/lecture_1/ReversePair.java
+++ src/lecture_1/ReversePair.java
@@ -2,4 +2,44 @@ package lecture_1;
 
 public class ReversePair {
 	
+	public static int countReversePair(int[] arr) {
+		return mergeSort(arr, 0, arr.length-1);
+	}
+	
+	private static int mergeSort(int[] arr, int l, int r) {
+		if (l == r) {
+			return 0;
+		}
+		
+		int m = l + ((r - l) >> 1);
+		
+		return mergeSort(arr, l, m) + mergeSort(arr, m+1, r) + merge(arr, l, m, r);
+	}
+	
+	private static int merge(int arr[], int l, int m, int r) {
+		int[] help = new int[r-l+1];
+		int index = 0;
+		int result = 0;
+		int p1 = l;
+		int p2 = m + 1;
+		
+		while (p1 <= m && p2 <= r) {
+			result += arr[p1] > arr[p2] ? (m-p1+1) : 0;
+			help[index++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+		}
+		
+		while (p1 <= m) {
+			help[index++] = arr[p1++];
+		}
+		
+		while (p2 <= r) {
+			help[index++] = arr[p2++];
+		}
+		
+		for (index = 0; index < help.length; index++) {
+			arr[l+index] = help[index];
+		}
+		
+		return result;
+	}
 }
--- src/run/DoRun.java
+++ src/run/DoRun.java
@@ -1,6 +1,7 @@
 package run;
 
 import lecture_1.MergeSort;
+import lecture_1.ReversePair;
 import lecture_1.SmallSum;
 
 public class DoRun {
@@ -9,8 +10,11 @@ public class DoRun {
 //		int[] arr = {4, 2, 3, 1 , 9, 5, 8};
 //		runMergeSort(arr);
 		
-		int[] arr = {1, 3, 4, 2, 5};
-		runSmallSum(arr);
+//		int[] arr = {1, 3, 4, 2, 5};
+//		runSmallSum(arr);
+		
+		int[] arr = {5, 4, 3, 2, 1};
+		runReversePair(arr);
 	}
 	
 	private static void printRes(int[] arr) {
@@ -26,6 +30,11 @@ public class DoRun {
 	
 	private static void runSmallSum(int[] arr) {
 		int result = SmallSum.smallSum(arr);
+		System.out.println(result);
+	}
+	
+	private static void runReversePair(int[] arr) {
+		int result = ReversePair.countReversePair(arr);
 		System.out.println(result);
 	}
 }
